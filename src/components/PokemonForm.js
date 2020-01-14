346c6793f949dc2e5460befb11d2f6ad03aa73cc
github username <github email address>
2020-01-14 14:23:29 -0600

    Done.

--- src/components/PokemonForm.js
+++ src/components/PokemonForm.js
@@ -13,6 +13,44 @@ class PokemonForm extends React.Component {
     }
   }
 
+  pushToAPI(){
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: this.state.name,
+        stats: [{value: this.state.hp, name: "hp"}],
+        sprites: {front: this.state.frontUrl,
+        back: this.state.backUrl}
+      })
+    });
+    let po = {"name": this.state.name, "stats": [{"value": this.state.hp, "name": "hp"}], "sprites": {"front": this.state.frontUrl, "back": this.state.backUrl}}
+    this.props.addPokemon(po)
+  }
+
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.setState({
+      name: e.target.name.value,
+      hp: e.target.hp.value,
+      frontUrl: e.target.frontUrl.value,
+      backUrl: e.target.backUrl.value
+    }, () => this.pushToAPI())
+  }
+  delete = () => {
+    fetch(`http://localhost:3000/pokemon/207`, {
+      method: 'delete'}).then(response =>
+        response.json().then(json => {
+          return json;
+        })
+      );
+      this.props.deletePokemon()
+  }
+
   render() {
     return (
       <div>
@@ -26,6 +64,7 @@ class PokemonForm extends React.Component {
           </Form.Group>
           <Form.Button>Submit</Form.Button>
         </Form>
+        <button onClick={this.delete}>Delete Last</button>
       </div>
     )
   }
