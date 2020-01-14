78f6db92f3346a073e26d935bebbbc13c84e2322
Marc Majcher <majcher@gmail.com>
2020-01-14 12:23:50 -0600

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -1,27 +1,47 @@
-import React from 'react'
-import { Card } from 'semantic-ui-react'
+import React from 'react';
+import { Card } from 'semantic-ui-react';
 
 class PokemonCard extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      showFront: true,
+    };
+  }
+
+  handleClick = e => {
+    this.setState({ showFront: !this.state.showFront });
+  };
+
   render() {
+    const pokemon = this.props.pokemon;
+    const hp = this.props.pokemon.stats.reduce(
+      (a, c) => (c.name === 'hp' ? c.value : a),
+      0
+    );
+
     return (
       <Card>
         <div>
-          <div className="image">
-            <img alt="oh no!" />
+          <div className="image" onClick={this.handleClick}>
+            <img
+              src={pokemon.sprites[this.state.showFront ? 'front' : 'back']}
+              alt={pokemon.name}
+            />
           </div>
           <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
+            <div className="header">{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</div>
           </div>
           <div className="extra content">
             <span>
               <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
+              {hp} hp
             </span>
           </div>
         </div>
       </Card>
-    )
+    );
   }
 }
 
-export default PokemonCard
+export default PokemonCard;
--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -1,23 +1,47 @@
-import React from 'react'
-import PokemonCollection from './PokemonCollection'
-import PokemonForm from './PokemonForm'
-import Search from './Search'
-import { Container } from 'semantic-ui-react'
+import React from 'react';
+import PokemonCollection from './PokemonCollection';
+import PokemonForm from './PokemonForm';
+import Search from './Search';
+import { Container } from 'semantic-ui-react';
 
 class PokemonPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      pokemon: [],
+      searchTerm: '',
+    };
+  }
+
+  loadPokemon = () => {
+    fetch('http://localhost:3000/pokemon')
+      .then(res => res.json())
+      .then(json => this.setState({ pokemon: json }));
+  };
+  componentDidMount() {
+    this.loadPokemon();
+  }
+
   render() {
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm onUpdate={this.loadPokemon} />
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search
+          value={this.state.searchTerm}
+          onChange={e => this.setState({ searchTerm: e.target.value })}
+        />
         <br />
-        <PokemonCollection />
+        <PokemonCollection
+          pokemon={this.state.pokemon.filter(e =>
+            e.name.includes(this.state.searchTerm)
+          )}
+        />
       </Container>
-    )
+    );
   }
 }
 
-export default PokemonPage
+export default PokemonPage;
